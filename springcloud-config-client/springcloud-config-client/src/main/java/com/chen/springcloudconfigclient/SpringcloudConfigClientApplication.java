823b7b909013b392808606874d7c37b1da4d5e78
chengchen <15355498770@163.com>
2017-12-21 18:53:43 +0800

    add spring cloud bus demo

---
+++ springcloud-config-client/springcloud-config-client/src/main/java/com/chen/springcloudconfigclient/SpringcloudConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.chen.springcloudconfigclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+
+@EnableEurekaClient
+@SpringBootApplication
+public class SpringcloudConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringcloudConfigClientApplication.class, args);
+	}
+}
